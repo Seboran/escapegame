6794336accfde41deef8e55f2628fc3af6a83369
MarieAlexandre <marie.alexandre@centrale-marseille.fr>
2018-03-24 02:23:47 +0100

    Modif de la fonction d'ajout d'élèves dans la salle 235
    La nouvelle fonction permet de choisir pour chaque côté de la salle quel rang on souhaite remplir d'élèves

--- simulation_salle_233_235.py
+++ simulation_salle_233_235.py
@@ -62,6 +62,8 @@ def Salle_233_235_porte_droite(x0,y0,largeur_porte):
     porte = Porte([x0+0.1+largeur_porte+5.8,y0-1], [x0+0.1+5.8+2*largeur_porte,y0-1])
     return murs_grd_salle, [porte]
 
+
+
 def tables_salle_233_235(x0,y0,largeur_porte,espace_x_porte,espace_x,espace_y,largeur_bureau,longueur_bureau):
     tables_Gs = []
     #ajout des tables dans la parte gauche de la salle
@@ -115,90 +117,49 @@ def tables_salle_233_235(x0,y0,largeur_porte,espace_x_porte,espace_x,espace_y,la
     return tables_Gs
 
 #==============================================================================
-#Fonctions de simulation des élèves dans la pièce
+#Fonction de simulation des élèves dans la pièce
 #==============================================================================
-def Salle_233_235_full_occupation(x0,y0,largeur_porte,espace_x_porte,espace_x,espace_y,largeur_bureau,longueur_bureau,espace_agent_table,sigma,epsilon):
+def Salle_233_235_occupation(x0,y0,largeur_porte,espace_x_porte,espace_x,espace_y,largeur_bureau,
+                                  longueur_bureau,espace_agent_table,rangs_gauche,rangs_droite,sigma,epsilon):
+    #les variables rangs_gauche et rangs_droite permettent d'indiquer sur quels rangs de la partie gauche et droite
+    #de la salle on souhaite placer des élèves
     eleves = []
-    for j in [0.25,0.75]:#A chaque bureau, on peut mettre un maximum de 2 élèves
-    #ajout des élèves dans la partie gauche de la salle (on place un agent à chaque place)
-        for i in range(3): 
-            x = x0+0.1+largeur_porte+espace_x_porte + largeur_bureau + i*(largeur_bureau + espace_x)
-            eleve = Agent(np.array([x+espace_agent_table,y0+j*longueur_bureau]), 1., sigma, epsilon*2, 'eleve')
-            eleves.append(eleve)
-        for i in range(4):
-             x = x0+0.1+largeur_porte+espace_x_porte + largeur_bureau + i*(largeur_bureau + espace_x)
+    for j in [0.25,0.75]:
+        #ajout des élèves dans la partie gauche de la salle (on place un agent à chaque place)
+        for i in rangs_gauche:
+            if(i !=4):
+                x = x0+0.1+largeur_porte+espace_x_porte + largeur_bureau + (i-1)*(largeur_bureau + espace_x)
+                eleve = Agent(np.array([x+espace_agent_table,y0+j*longueur_bureau]), 1., sigma, epsilon*2, 'eleve')
+                eleves.append(eleve)
+        for i in rangs_gauche:
+             x = x0+0.1+largeur_porte+espace_x_porte + largeur_bureau + (i-1)*(largeur_bureau + espace_x)
              y = y0+longueur_bureau+espace_y+j*longueur_bureau 
              eleve = Agent(np.array([x+espace_agent_table,y]), 1., sigma, epsilon*2, 'eleve')
              eleves.append(eleve)
-        for i in range(4):
-             x = x0+0.1+largeur_porte+espace_x_porte + largeur_bureau + i*(largeur_bureau + espace_x)
+        for i in rangs_gauche:
+             x = x0+0.1+largeur_porte+espace_x_porte + largeur_bureau + (i-1)*(largeur_bureau + espace_x)
              y = y0+longueur_bureau+espace_y+j*longueur_bureau + longueur_bureau 
              eleve = Agent(np.array([x+espace_agent_table,y]), 1., sigma, epsilon*2, 'eleve')
              eleves.append(eleve)
         #ajout des élèves dans la partie droite de la salle (on place un agent à chaque place)
-        for i in range(3):
+        for i in rangs_droite:
             x =  x0+0.1+5.8+2*largeur_porte+4.2 - j*longueur_bureau
-            y =  y0 + espace_y + i*(largeur_bureau + espace_y) +largeur_bureau
+            y =  y0 + espace_y + (i-1)*(largeur_bureau + espace_y) + largeur_bureau
             eleve = Agent(np.array([x,y + espace_agent_table]), 1., sigma, epsilon*2, 'eleve')
             eleves.append(eleve)
-        for i in range(3):
+        for i in rangs_droite:
              x = x0+0.1+5.8+2*largeur_porte+4.2 - (espace_x+2*longueur_bureau) - j*longueur_bureau
-             y =  y0 + espace_y + i*(largeur_bureau + espace_y) +largeur_bureau
+             y =  y0 + espace_y + (i-1)*(largeur_bureau + espace_y) + largeur_bureau
              eleve = Agent(np.array([x,y + espace_agent_table]), 1., sigma, epsilon*2, 'eleve')
              eleves.append(eleve)
-        for i in range(3):
+        for i in rangs_droite:
             x = x0+0.1+5.8+2*largeur_porte+4.2 - (espace_x+longueur_bureau) - j*longueur_bureau
-            if(i == 2):
+            if((i-1) == 2):
                 x = x0+0.1+5.8+2*largeur_porte+4.2 - longueur_bureau - j*longueur_bureau
-            y =  y0 + espace_y + i*(largeur_bureau + espace_y) +largeur_bureau
+            y =  y0 + espace_y + (i-1)*(largeur_bureau + espace_y) + largeur_bureau
             eleve = Agent(np.array([x,y + espace_agent_table]), 1., sigma, epsilon*2, 'eleve')
             eleves.append(eleve)
-    
-    return eleves
-
-def Salle_233_235_left_occupation(x0,y0,largeur_porte,espace_x_porte,espace_x,espace_y,largeur_bureau,longueur_bureau,espace_agent_table,sigma,epsilon):
-    eleves = []
-    for j in [0.25,0.75]:#A chaque bureau, on peut mettre un maximum de 2 élèves
-    #ajout des élèves dans la partie gauche de la salle (on place un agent à chaque place)
-        for i in range(3): 
-            x = x0+0.1+largeur_porte+espace_x_porte + largeur_bureau + i*(largeur_bureau + espace_x)
-            eleve = Agent(np.array([x+espace_agent_table,y0+j*longueur_bureau]), 1., sigma, epsilon*2, 'eleve')
-            eleves.append(eleve)
-        for i in range(4):
-             x = x0+0.1+largeur_porte+espace_x_porte + largeur_bureau + i*(largeur_bureau + espace_x)
-             y = y0+longueur_bureau+espace_y+j*longueur_bureau 
-             eleve = Agent(np.array([x+espace_agent_table,y]), 1., sigma, epsilon*2, 'eleve')
-             eleves.append(eleve)
-        for i in range(4):
-             x = x0+0.1+largeur_porte+espace_x_porte + largeur_bureau + i*(largeur_bureau + espace_x)
-             y = y0+longueur_bureau+espace_y+j*longueur_bureau + longueur_bureau 
-             eleve = Agent(np.array([x+espace_agent_table,y]), 1., sigma, epsilon*2, 'eleve')
-             eleves.append(eleve)
-             
-    return eleves
-
-def Salle_233_235_right_occupation(x0,y0,largeur_porte,espace_x_porte,espace_x,espace_y,largeur_bureau,longueur_bureau,espace_agent_table,sigma,epsilon):
-    eleves = []
-    for j in [0.25,0.75]:#A chaque bureau, on peut mettre un maximum de 2 élèves
-        #ajout des élèves dans la partie droite de la salle (on place un agent à chaque place)
-        for i in range(3):
-            x =  x0+0.1+5.8+2*largeur_porte+4.2 - j*longueur_bureau
-            y =  y0 + espace_y + i*(largeur_bureau + espace_y) +largeur_bureau
-            eleve = Agent(np.array([x,y + espace_agent_table]), 1., sigma, epsilon*2, 'eleve')
-            eleves.append(eleve)
-        for i in range(3):
-             x = x0+0.1+5.8+2*largeur_porte+4.2 - (espace_x+2*longueur_bureau) - j*longueur_bureau
-             y =  y0 + espace_y + i*(largeur_bureau + espace_y) +largeur_bureau
-             eleve = Agent(np.array([x,y + espace_agent_table]), 1., sigma, epsilon*2, 'eleve')
-             eleves.append(eleve)
-        for i in range(3):
-            x = x0+0.1+5.8+2*largeur_porte+4.2 - (espace_x+longueur_bureau) - j*longueur_bureau
-            if(i == 2):
-                x = x0+0.1+5.8+2*largeur_porte+4.2 - longueur_bureau - j*longueur_bureau
-            y =  y0 + espace_y + i*(largeur_bureau + espace_y) +largeur_bureau
-            eleve = Agent(np.array([x,y + espace_agent_table]), 1., sigma, epsilon*2, 'eleve')
-            eleves.append(eleve)
-    
-    return eleves
+            
+        return eleves
     
 
