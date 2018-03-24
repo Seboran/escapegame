b6303aed8907905fb4e35a040a50163fd0d008bb
MarieAlexandre <marie.alexandre@centrale-marseille.fr>
2018-03-24 01:48:56 +0100

    Ajout simulation salle 235 1 porte (droite ou gauche)

--- simulation_salle_233_235.py
+++ simulation_salle_233_235.py
@@ -6,13 +6,13 @@ Created on Sat Mar 24 00:07:26 2018
 """
 
 import numpy as np
-from utils import Obstacle, Agent
+from utils import *
 
 
 #==============================================================================
 #Fonctions de simulation de la pièce
 #==============================================================================
-def Salle_233_235_deux_portes(x0,y0,largeur_porte,espace_x_porte,espace_x,espace_y,largeur_bureau,longueur_bureau):
+def Salle_233_235_deux_portes(x0,y0,largeur_porte):
     murs_grd_salle = []
     murs_grd_salle.append(Obstacle([[x0,y0],[x0+0.1,y0]]))
     murs_grd_salle.append(Obstacle([[x0+0.1+largeur_porte,y0],[x0+0.1+largeur_porte+5.8,y0]]))
@@ -23,7 +23,46 @@ def Salle_233_235_deux_portes(x0,y0,largeur_porte,espace_x_porte,espace_x,espace
     murs_grd_salle.append(Obstacle([[x0+0.1,y0],[x0+0.1,y0+0.8]]))
     murs_grd_salle.append(Obstacle([[x0+0.1+largeur_porte+5.8,y0],[x0+0.1+largeur_porte+5.8,y0+0.8]]))
     murs_grd_salle.append(Obstacle([[x0+5.9,y0+5.8],[x0+5.9,y0+5.6]]))
+    
+    #simulation des portes
+    porte_1 = Porte([x0+0.1,y0-1], [x0+0.1+largeur_porte,y0-1])
+    porte_2 = Porte([x0+0.1+largeur_porte+5.8,y0-1], [x0+0.1+5.8+2*largeur_porte,y0-1])
+    return murs_grd_salle, [porte_1,porte_2]
 
+
+
+def Salle_233_235_porte_gauche(x0,y0,largeur_porte):
+    murs_grd_salle = []
+    murs_grd_salle.append(Obstacle([[x0,y0],[x0+0.1,y0]]))
+    murs_grd_salle.append(Obstacle([[x0+0.1+largeur_porte,y0],[x0+0.1+largeur_porte+10+largeur_porte,y0]]))
+    murs_grd_salle.append(Obstacle([[x0+0.1+5.8+2*largeur_porte+4.2,y0],[x0+0.1+5.8+2*largeur_porte+4.2,y0+5.8]]))
+    murs_grd_salle.append(Obstacle([[x0,y0],[x0,y0+5.8]]))
+    murs_grd_salle.append(Obstacle([[x0,y0+5.8],[x0+0.1+5.8+2*largeur_porte+4.2,y0+5.8]]))
+    murs_grd_salle.append(Obstacle([[x0+0.1,y0],[x0+0.1,y0+0.8]]))
+    murs_grd_salle.append(Obstacle([[x0+5.9,y0+5.8],[x0+5.9,y0+5.6]]))
+    
+    #simulation des portes
+    porte = Porte([x0+0.1,y0-1], [x0+0.1+largeur_porte,y0-1])
+    return murs_grd_salle, [porte]
+
+
+
+def Salle_233_235_porte_droite(x0,y0,largeur_porte):
+    murs_grd_salle = []
+    murs_grd_salle.append(Obstacle([[x0,y0],[x0+0.1+largeur_porte+5.8,y0]]))
+
+    murs_grd_salle.append(Obstacle([[x0+0.1+5.8+2*largeur_porte,y0],[x0+0.1+5.8+2*largeur_porte+4.2,y0]]))
+    murs_grd_salle.append(Obstacle([[x0+0.1+5.8+2*largeur_porte+4.2,y0],[x0+0.1+5.8+2*largeur_porte+4.2,y0+5.8]]))
+    murs_grd_salle.append(Obstacle([[x0,y0],[x0,y0+5.8]]))
+    murs_grd_salle.append(Obstacle([[x0,y0+5.8],[x0+0.1+5.8+2*largeur_porte+4.2,y0+5.8]]))
+    murs_grd_salle.append(Obstacle([[x0+0.1+largeur_porte+5.8,y0],[x0+0.1+largeur_porte+5.8,y0+0.8]]))
+    murs_grd_salle.append(Obstacle([[x0+5.9,y0+5.8],[x0+5.9,y0+5.6]]))
+    
+    #simulation des portes
+    porte = Porte([x0+0.1+largeur_porte+5.8,y0-1], [x0+0.1+5.8+2*largeur_porte,y0-1])
+    return murs_grd_salle, [porte]
+
+def tables_salle_233_235(x0,y0,largeur_porte,espace_x_porte,espace_x,espace_y,largeur_bureau,longueur_bureau):
     tables_Gs = []
     #ajout des tables dans la parte gauche de la salle
     #simulation des tables collées au mur 
@@ -72,10 +111,8 @@ def Salle_233_235_deux_portes(x0,y0,largeur_porte,espace_x_porte,espace_x,espace
         y2 = y0 + espace_y + largeur_bureau +i*(largeur_bureau + espace_y)
     
         tables_Gs = tables_Gs + [Obstacle([[x1,y1],[x1,y2]]), Obstacle([[x1,y2],[x2,y2]]),
-                                 Obstacle([[x2,y2],[x2,y1]]), Obstacle([[x2,y1],[x1,y1]])]  
-        
-    obstacles = murs_grd_salle + tables_Gs
-    return obstacles
+                                 Obstacle([[x2,y2],[x2,y1]]), Obstacle([[x2,y1],[x1,y1]])]         
+    return tables_Gs
 
 #==============================================================================
 #Fonctions de simulation des élèves dans la pièce
@@ -119,4 +156,49 @@ def Salle_233_235_full_occupation(x0,y0,largeur_porte,espace_x_porte,espace_x,es
     
     return eleves
 
+def Salle_233_235_left_occupation(x0,y0,largeur_porte,espace_x_porte,espace_x,espace_y,largeur_bureau,longueur_bureau,espace_agent_table,sigma,epsilon):
+    eleves = []
+    for j in [0.25,0.75]:#A chaque bureau, on peut mettre un maximum de 2 élèves
+    #ajout des élèves dans la partie gauche de la salle (on place un agent à chaque place)
+        for i in range(3): 
+            x = x0+0.1+largeur_porte+espace_x_porte + largeur_bureau + i*(largeur_bureau + espace_x)
+            eleve = Agent(np.array([x+espace_agent_table,y0+j*longueur_bureau]), 1., sigma, epsilon*2, 'eleve')
+            eleves.append(eleve)
+        for i in range(4):
+             x = x0+0.1+largeur_porte+espace_x_porte + largeur_bureau + i*(largeur_bureau + espace_x)
+             y = y0+longueur_bureau+espace_y+j*longueur_bureau 
+             eleve = Agent(np.array([x+espace_agent_table,y]), 1., sigma, epsilon*2, 'eleve')
+             eleves.append(eleve)
+        for i in range(4):
+             x = x0+0.1+largeur_porte+espace_x_porte + largeur_bureau + i*(largeur_bureau + espace_x)
+             y = y0+longueur_bureau+espace_y+j*longueur_bureau + longueur_bureau 
+             eleve = Agent(np.array([x+espace_agent_table,y]), 1., sigma, epsilon*2, 'eleve')
+             eleves.append(eleve)
+             
+    return eleves
+
+def Salle_233_235_right_occupation(x0,y0,largeur_porte,espace_x_porte,espace_x,espace_y,largeur_bureau,longueur_bureau,espace_agent_table,sigma,epsilon):
+    eleves = []
+    for j in [0.25,0.75]:#A chaque bureau, on peut mettre un maximum de 2 élèves
+        #ajout des élèves dans la partie droite de la salle (on place un agent à chaque place)
+        for i in range(3):
+            x =  x0+0.1+5.8+2*largeur_porte+4.2 - j*longueur_bureau
+            y =  y0 + espace_y + i*(largeur_bureau + espace_y) +largeur_bureau
+            eleve = Agent(np.array([x,y + espace_agent_table]), 1., sigma, epsilon*2, 'eleve')
+            eleves.append(eleve)
+        for i in range(3):
+             x = x0+0.1+5.8+2*largeur_porte+4.2 - (espace_x+2*longueur_bureau) - j*longueur_bureau
+             y =  y0 + espace_y + i*(largeur_bureau + espace_y) +largeur_bureau
+             eleve = Agent(np.array([x,y + espace_agent_table]), 1., sigma, epsilon*2, 'eleve')
+             eleves.append(eleve)
+        for i in range(3):
+            x = x0+0.1+5.8+2*largeur_porte+4.2 - (espace_x+longueur_bureau) - j*longueur_bureau
+            if(i == 2):
+                x = x0+0.1+5.8+2*largeur_porte+4.2 - longueur_bureau - j*longueur_bureau
+            y =  y0 + espace_y + i*(largeur_bureau + espace_y) +largeur_bureau
+            eleve = Agent(np.array([x,y + espace_agent_table]), 1., sigma, epsilon*2, 'eleve')
+            eleves.append(eleve)
+    
+    return eleves
+    
 
